0efac7fa9a5cd9098dd0eda4ab3f31436d32e806
DestinyFrog <petrocalixto5@hotmail.com>
2022-06-29 15:15:10 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,82 +1,105 @@
-﻿//Console.WriteLine("Hello, world!");
-namespace letterGame {
-
+﻿namespace letterGame {
 //classes e estruturas instanciadas com informações do jogo
 //classe que guarda variaveis e métodos sobre os niveis
 public class Level {
     public int[] Map = new int[72];     //array de valores inteiros referente ao mapa
     public int win;                     //ponto do array onde esta a linha de chegada
     public int maxSteps;                //numero maximo de passos que podem ser dados
-
+    public int start;                   //ponto de inicio do Player
     //método construtor que dá valor as variaveis acima
-    public Level (int[] Map, int win, int maxSteps){
+    public Level (int[] Map, int win, int maxSteps, int start){
         this.Map = Map;
         this.win = win;
-        this.maxSteps = maxSteps;}}
-
+        this.maxSteps = maxSteps;
+        this.start = start;}}
 //classe que guarda variaveis e métodos sobre o Player
 public class Player{
-    private int startPosition;          //posiçao inicial do Player
+    public string sprite = "A";         //sprite do Player
     public int updatePosition;          //posicao atual do Player
     public int actualSteps;             //numero de passos dado pelo Player
 
     //método construtor que da valor as variaveis acima
     public Player(int startPosition){
-        this.startPosition = startPosition;
         this.updatePosition = startPosition;}
-    
+    public void changeSprite(string newSprite){
+        this.sprite = newSprite;
+    }
     //método que atualiza o movimento do player de acordo com a tecla pressionada
     public void updatePlayer(string key, Level lvl){
+        if(this.actualSteps > lvl.maxSteps){
+            Game.Lose();
+        }else if(this.updatePosition == lvl.win){
+            Game.Win();
+        }else{
         switch (key){
             case "A":
                 if(lvl.Map[this.updatePosition - 1] != 3){
                     this.updatePosition--;
-                    actualSteps++;
-                }
+                } actualSteps++;
                 break;
             case "D":
                 if(lvl.Map[this.updatePosition + 1] != 3){
                     this.updatePosition++;
-                    actualSteps++;
-                }
+                } actualSteps++;
                 break;
             case "S":
                 if(lvl.Map[this.updatePosition + 12] != 3){
                     this.updatePosition = this.updatePosition + 12;
-                    actualSteps++;
-                }
+                } actualSteps++;
                 break;
             case "W":
                 if(lvl.Map[this.updatePosition - 12] != 3){
                     this.updatePosition = this.updatePosition - 12;
-                    actualSteps++;
-                }
+                } actualSteps++;
                 break;
             default:
-                Console.ReadLine();
+                Game.Lose();
                 break;
-        }
+        }}
     }}
-
+//
+public class Enemie{
+    //public int 
+}
 //      __________________________
 //      classe que controla o jogo
 public class Game{
-    //instancia da variavel da fase
-    public static Level lvl1 = new Level(new int[72]{
+    public static int index = 0;
+    public static Level[] levels = new Level[3] {
+        new Level(new int[72]{
         3,3,3,3,3,3,3,3,3,3,3,4,
-        3,1,3,1,1,1,3,1,1,1,3,4,
+        3,6,3,1,1,1,3,1,1,1,3,4,
         3,1,3,1,3,1,3,1,3,1,3,4,
         3,1,3,1,3,1,3,1,3,5,3,4,
         3,1,1,1,3,1,1,1,3,1,3,4,
-        3,3,3,3,3,3,3,3,3,3,3,4}, 45, 23);
-    //instancia da variavel do Player
-    public static Player pl = new Player(13);
+        3,3,3,3,3,3,3,3,3,3,3,4}, 45, 23, 13)
+        ,
+        new Level(new int[72]{
+        3,3,3,3,3,3,3,3,3,3,3,4,
+        3,6,1,1,1,3,3,1,1,1,3,4,
+        3,3,3,3,1,1,1,1,3,1,3,4,
+        3,5,1,3,3,3,3,3,3,1,3,4,
+        3,3,1,1,1,1,1,1,1,1,3,4,
+        3,3,3,3,3,3,3,3,3,3,3,4}, 37, 23, 13)
+        ,
+        new Level(new int[72]{
+        3,3,3,3,3,3,3,3,3,3,3,4,
+        3,6,1,1,1,1,1,1,1,1,3,4,
+        3,1,1,1,1,1,1,1,1,1,3,4,
+        3,5,1,1,1,1,1,1,1,1,3,4,
+        3,1,1,1,1,1,1,1,1,1,3,4,
+        3,3,3,3,3,3,3,3,3,3,3,4}, 37, 23, 13)
+    };
+
+    //Variaveis de uso geral
+    public static Level mainLevel = levels[index];
+    public static Player pl = new Player(mainLevel.start); //instancia da variavel do Player
 
     //método principal de açao
     public static void Main(string[] args){
         top:
-        BuildScreen(lvl1.Map);
-        pl.updatePlayer(Convert.ToString(Console.ReadKey().Key), lvl1);
+        BuildScreen(mainLevel.Map);
+        pl.updatePlayer(Convert.ToString(Console.ReadKey().Key), mainLevel);
         goto top;
     }
     //método que limpa a tela, e pinta ela denovo 
@@ -84,45 +107,75 @@ public class Game{
         Console.Clear();  //limpa a tela
 
         //estrutura de repetição de cada digito do mapa
-        for(int i = 0; i < lvl1.Map.Length; i += 1){ 
+        for(int i = 0; i < mainLevel.Map.Length; i += 1){ 
             if(i == pl.updatePosition){                             //se o digito, for igual a posiçao do player,
                 Console.Write(Plot.NumberToLetter(2));              //o local se torna o Player,
             }else{                                                  //caso contrario,
-                Console.Write(Plot.NumberToLetter(lvl1.Map[i]));    //o local se tornara o digito padrao.
+                Console.Write(Plot.NumberToLetter(mainLevel.Map[i]));    //o local se tornara o digito padrao.
             }
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
         //escreve na tela o número de passos
-        Console.WriteLine("numero de passos: " + pl.actualSteps + @"\" + lvl1.maxSteps);
+        Console.WriteLine("numero de passos: " + pl.actualSteps + @"\" +  mainLevel.maxSteps);
     }
+
+    //  Método de ação ao ganhar
+    public static void Win(){
+        Console.WriteLine("\n Voce venceu, Parabéns");
+        Console.ReadKey();
+        Console.Clear();
+        if(index < levels.Length -1){
+            NextLevel();
+        }else{
+            End();}}
+    public static void Lose(){
+        Console.WriteLine("\n Voce perdeu ???");
+        Console.WriteLine(" Sério isso '-'");
+        Console.ReadKey();
+        Console.Clear();
+        Environment.Exit(0);}
+
+    public static void End(){
+        Console.WriteLine("\n Fim do jogo");
+        Console.ReadLine();
+        Console.WriteLine(" A senha é C4L1GUL4.");
+        Console.ReadKey();
+        Console.Clear();
+        Environment.Exit(0);}
+
+    public static void NextLevel(){
+        index++;
+        mainLevel = levels[index];
+        pl.actualSteps = 0;
+        pl.updatePosition = mainLevel.start;
+        Console.Clear();
+        Console.WriteLine(" Level " + (index+1));
+        Console.ReadKey();}
 }
 
 //classe com métodos e variaveis do sistema que opera o jogo
 public class Plot{
+    //método que converte 
     public static string NumberToLetter(int number){
         switch(number){
             case 1:
-                return " . ";
-                break;
+                return "   ";
             case 2:
-                return " A ";
-                break;
+                return " " + Game.pl.sprite + " ";
             case 3:
-                return " # ";
-                break;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.Black;
+                return "=#=";
             case 4:
                 return "\n";
-                break;
             case 5:
-                return " ^ ";
-                break;
+                return " B ";
             case 6:
-                return " V ";
-                break;
+                return " A ";
             default:
                 return "   ";
-                break;
-            }
-        }
+        }}
     }
 }
